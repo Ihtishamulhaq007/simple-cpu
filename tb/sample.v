a5069ee9602d344a66f1dd64fc9c85c115334556
Ihtishamulhaq007 <007ihtishamulhaq@gmail.com>
2026-06-13 16:33:02 +0530

    new file:   .gitignore 	new file:   docs/Logs/Day1.txt 	new file:   rtl/sample.v 	new file:   tb/sample.v

---
+++ tb/sample.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+
+module alu_tb;
+
+reg  [3:0] a;
+reg  [3:0] b;
+wire [3:0] y;
+
+alu dut (
+    .a(a),
+    .b(b),
+    .y(y)
+);
+
+initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, alu_tb);
+
+    a = 4'd3; b = 4'd2;
+    #10;
+
+    a = 4'd5; b = 4'd1;
+    #10;
+
+    $finish;
+end
+
+endmodule
